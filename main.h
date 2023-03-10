1d186f2d124ad189eacf436a1aec3c48e7dd7265
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-03-09 18:26:35 -0800

    push

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _printf(const char *format, ...);
+int print_int(va_list i);
+int print_dec(va_list d);
+int _putchar(char c);
+int print_char(va_list c);
+int print_string(va_list s);
+int print_bin(va_list b);
+
+#endif
