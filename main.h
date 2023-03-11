95672b36f37ce77c58e29ebd9d4b35b4916cfbc1
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-03-11 12:31:10 -0800

    Cameron

--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h>
 int _printf(const char *format, ...);
 int print_int(va_list i);
 int print_dec(va_list d);
@@ -8,5 +9,17 @@ int _putchar(char c);
 int print_char(va_list c);
 int print_string(va_list s);
 int print_bin(va_list b);
+
+
+/**
+ * @sc: specifier
+ * @f: function
+ */
+
+typedef struct code_format
+{
+	char *sc;
+	int (*f)(va_list);
+} code_f;
 
 #endif
