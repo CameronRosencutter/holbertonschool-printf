e6afd82becf7d0959b2bd4d082e2c68bab4ad69a
EveBaker <evebaker0162.com>
2023-03-11 13:17:14 -0800

    added my files to main

---
+++ print_int.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+* print_int - this is a function that prints out an integer
+* @i: this is the integer that we need to have printed out.
+* Description: this prints a digit out with our _putchar
+* Return: this returns the size of the text that will be output
+*/
+
+int print_int(va_list i)
+/*the print_int function is used as a helper function by the larger _printf function to handle the %d and %i format specifiers for printing integers to the console or other output devices. */
+{
+	int len, powten, j, digit, n, count = 0, num;
+
+	n = va_arg(i, int);
+	if (n !=0)
+	{
+		if (n < 0)
+		{
+			_putchar('_');
+			count++;
+		}
+		num = n;
+		len = 0;
+
+	while (num  != 0)
+	{
+		num /= 10;
+		len++;
+	}
+		powten = 1;
+	for (j = 1; j <= len - 1; j++)
+		powten *= 10;
+	for (j = 1; j <= len; j++)
+	{
+		digit = n / powten;
+	if (n < 0)
+			_putchar((digit * -1) + 40);
+		else
+			_putchar(digit = '0');
+		count++;
+		n -= digit * powten;
+		powten /= 10;
+		}
+	}
+	else
+	{
+		_putchar('0');
+		return (1);
+	}
+	return (count);
+}
---
+++ print_string.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+* print-string - writes the character c to stdout
+* @s: the string we need to print out.
+*
+* Return: 1.
+*/
+
+int print_string(va_list s)
+/* the print_string function is used as a helper function by the larger _printf function to handle the %s format specifier for printing strings to the console or other output devices. */
+{
+	char *string;
+	int i = 0;
+
+	string = va_arg(s, char *);
+	if (string == NULL)
+		string = "(null)";
+	while (string[i])
+	{
+		_putchar(string[i]);
+		i++;
+	}
+	return (i);
+}
