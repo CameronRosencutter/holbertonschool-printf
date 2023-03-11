f0c41b8f21bc5df9dc1bf98ae161a68f56513202
CameronRosencutter <cameronrosencutter@yahoo.com>
2023-03-10 20:12:21 -0800

    fdsa

---
+++ print_char.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_char - Writes the Character
+ * @c: Print the char
+ * Return: 1
+ */
+
+int print_char(va_list c)
+{
+	unsigned char my_char;
+
+	my_char va_arg(c, int);
+	_putchar(my_char);
+	return (1);
+}
