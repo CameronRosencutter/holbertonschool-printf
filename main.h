d9216b91b6e73b16e03a9140e71298b43a7fd966
Cameron <120847793+CameronRosencutter@users.noreply.github.com>
2023-07-10 17:39:57 -0500

    Update main.h

--- main.h
+++ main.h
@@ -9,28 +9,11 @@
 #include <stdarg.h>
 #include <unistd.h>
 
-/* STRUCTS */
-/**
- * struct print - printf type with corresponding print function
- * @t: print type
- * @f: print function
- */
-typedef struct print
-{
-	char *t;
-	int (*f)(va_list);
-} print_t;
-
 /* Prototypes */
 
-int _putchar(char c);
-int _printf(const char *format, ...);
-int print_c(va_list c);
-int print_s(va_list s);
-int print_p(va_list p);
-int print_i(va_list i);
-int print_d(va_list d);
-int print_b(va_list b);
+int print_char(va_list ap);
+int print_str(va_list ap);
+int print_int(va_list ap);
 int print_unsigned_int(unsigned int n);
 
 #endif /* MAIN_H */
