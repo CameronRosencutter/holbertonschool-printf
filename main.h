e325dee8513a949bc26e369fda3eb7c30e069724
Cameron <120847793+CameronRosencutter@users.noreply.github.com>
2023-07-10 17:50:48 -0500

    Update main.h

--- main.h
+++ main.h
@@ -19,6 +19,12 @@ int _pow_recursion(int x, int y);
 int (*check(const char *format))(va_list);
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_c(va_list c);
+int print_s(va_list s);
+int print_p(va_list p);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_b(va_list b);
 
 /* STRUCTS */
 /**
