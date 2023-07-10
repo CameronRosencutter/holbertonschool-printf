d98a9d5bb3d845a6028e6fa046b44d8669d2a272
Cameron <120847793+CameronRosencutter@users.noreply.github.com>
2023-07-10 18:00:27 -0500

    Create printf_function.c

---
+++ printf_function.c
@@ -0,0 +1,52 @@
+#include "main.h"
+
+/**
+* print_c - function
+* @c: Char
+* Description: print char
+* Return: 1, always
+*/
+
+int print_c(va_list c)
+{
+	char ch = (char)va_arg(c, int);
+	{
+	_putchar(ch);
+	}
+	return (1);
+}
+
+/**
+* print_s - function
+* @s: string
+* Description: prints string
+* Return: numbers and characters printed
+*/
+
+int print_s(va_list s)
+{
+	int count;
+	char *str = va_arg(s, char *);
+
+	if (!str)
+	str = "(null)";
+	for (count = 0; str[count]; count++)
+	{
+		_putchar(str[count]);
+	}
+return (count);
+}
+
+/**
+* print_p - function
+* @p: arguments
+* Description: prints percent symbol
+* Return: characters printed
+*/
+
+int print_p(va_list p)
+{
+	(void)p;
+	_putchar('%');
+	return (1);
+}
