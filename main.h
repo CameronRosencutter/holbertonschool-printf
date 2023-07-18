67151c120ff799a257ef31a87ed9a6b1dbebf519
Muncherxyz <19kortlanblocker@gmail.com>
2023-07-18 08:32:59 -0700

    removedDuplicates

--- main.h
+++ main.h
@@ -11,11 +11,6 @@
 
 /* Prototypes */
 
-int print_char(va_list ap);
-int print_str(va_list ap);
-int print_int(va_list ap);
-int print_unsigned_int(unsigned int n);
-int _pow_recursion(int x, int y);
 int (*check(const char *format))(va_list);
 int _printf(const char *format, ...);
 int _putchar(char c);
